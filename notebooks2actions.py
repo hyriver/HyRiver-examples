e425c8638b3a8d1f819cf7af6e73a624743826cd
Taher Chegini <cheginit@gmail.com>
2024-11-19 14:38:51 -0500

    LNT: Run pre-commit. [skip ci]

--- notebooks2actions.py
+++ notebooks2actions.py
@@ -5,9 +5,10 @@ import fileinput
 from pathlib import Path
 
 nb_list = sorted(f.stem for f in Path("notebooks").glob("*.ipynb") if "nid_dib" not in f.stem)
-for line in fileinput.input(".github/workflows/test.yml", inplace=True):
-    if "notebook: [" in line:
-        print(f"        notebook: {nb_list}", end="")
-        print()
-    else:
-        print(line, end="")
+with fileinput.input(".github/workflows/test.yml", inplace=True) as f:
+    for line in f:
+        if "notebook: [" in line:
+            print(f"        notebook: {nb_list}", end="")
+            print()
+        else:
+            print(line, end="")
